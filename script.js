22abee336fa45304d99092c31775d6d961de2d18
user <valeriazhukovskaya789@gmail.com>
2023-01-25 22:08:08 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById("btnProduct").onclick = function() {
+    calcCost();
+};
+
+function calcCost() {
+    var sP = document.getElementById("")
+    document.getElementById("infoProduct").style.display = "block"
+}
